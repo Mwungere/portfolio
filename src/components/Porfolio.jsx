8ba37a19435e1930b4f8619f34b1848d518dc645
Mwungere <mwungereelite56@gmail.com>
2023-07-22 11:02:55 +0200

    end of portfolio

--- src/components/Porfolio.jsx
+++ src/components/Porfolio.jsx
@@ -13,27 +13,34 @@ const Portfolio = () => {
     const portfolios = [
         {
             id: 1,
-            src: cyclist
+            src: cyclist,
+            button: 'React, TailwindCss, Mongodb, nodejs, nextjs'
         },
         {
             id: 2,
-            src: shoesEcommerce
+            src: shoesEcommerce,
+            button: 'Html, Bootstrapcss, javascript, Mongodb, nodejs, nextjs'
+
         },
         {
             id: 3,
-            src: timeLog
+            src: timeLog,
+            button: 'React, materialisecss, Tailwind, Mongodb, nodejs, nextjs'
         },
         {
             id: 4,
-            src: personFashion
+            src: personFashion,
+            button: 'React, Tailwind, Firebase, nodejs, nextjs'
         },
         {
             id: 5,
-            src: dashBoard
+            src: dashBoard,
+            button: 'React, Tailwind, MYSQL, nodejs, nextjs'
         },
         {
             id: 6,
-            src: weather
+            src: weather,
+            button: 'React, CSS, Mongodb, nodejs, nextjs'
         }
 
     ]
@@ -48,14 +55,14 @@ const Portfolio = () => {
 
                 <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                     {
-                        portfolios.map(({ id, src }) => {
+                        portfolios.map(({ id, src, button }) => {
                             return (
 
                     <div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
                         <img src={src} alt="" className=" rounded-md duration-200 hover:scale-105" />
                         <div className=" flex items-center justify-center">
-                            <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
-                            <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+                            <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">{button}</button>
+                            {/* <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button> */}
                         </div>
                     </div>
                             
